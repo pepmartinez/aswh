314dcc75ba84d2c50183eea4997156c94741a3fc
pepe <pep.martinez@gmail.com>
2020-12-01 19:47:05 +0100

    added fixed window for consumers

--- app.js
+++ app.js
@@ -70,22 +70,28 @@ module.exports = function  (opts, context, done) {
     });
   });
 
+  function delay (req, res, next) {
+    const delay = parseInt(req.query.d);
+    if (delay <= 0) return next();
+    log.verbose ('enforce delay of %d', delay);
+    setTimeout (() => next (), delay);
+  }
 
   // test responses for various http response codes
-  app.all ('/test/200', (req, res) => res.status (200).send ('a 200'));
-  app.all ('/test/400', (req, res) => res.status (400).send ('a 400'));
-  app.all ('/test/404', (req, res) => res.status (404).send ('a 404'));
-  app.all ('/test/500', (req, res) => res.status (500).send ('a 500'));
+  app.all ('/test/200', [delay], (req, res) => res.status (200).send ('a 200'));
+  app.all ('/test/400', [delay], (req, res) => res.status (400).send ('a 400'));
+  app.all ('/test/404', [delay], (req, res) => res.status (404).send ('a 404'));
+  app.all ('/test/500', [delay], (req, res) => res.status (500).send ('a 500'));
 
   // do not respond
-  app.all ('/test/noresponse', (req, res) => {});
+  app.all ('/test/noresponse', [delay],  (req, res) => {});
 
   // close socket
-  app.all ('/test/drop', (req, res) => req.socket.destroy());
+  app.all ('/test/drop', [delay], (req, res) => req.socket.destroy());
 
   // express error manager
   app.use (function (err, req, res, next) {
-    console.error ('error caught: %s', err.stack);
+    log.error ('error caught: %s', err.stack);
     res.status (err.status || 500).send (err.stack);
   });
 
